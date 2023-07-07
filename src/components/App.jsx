6f58a39bb57944122589b45b0d3b57b5e078d0ab
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-07-07 13:55:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+
+import initialContact from './dataBase/initialContacts';
+
+import nanoId from 'nano-id';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: initialContact,
+    filter: '',
+  };
+
+  addContact = data => {
+    const newContact = {
+      id: nanoId(),
+      ...data,
+    };
+    this.state.contacts.map(contact => contact.name).includes(data.name)
+      ? alert(`${data.name} is already in contact`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+  };
+
+  findContact = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  filter = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+  handleDelete = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div className={css.appContainer}>
+        <Section title="Phonebook">
+          <ContactForm addContact={this.addContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter value={this.state.filter} onChange={this.findContact} />
+          <ContactList
+            contacts={this.filter()}
+            handleDelete={this.handleDelete}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
